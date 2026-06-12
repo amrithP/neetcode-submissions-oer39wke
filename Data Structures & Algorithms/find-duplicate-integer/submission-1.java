edc9a9b561579dab05413aa209c16a84165a69ef
amrithP <amrithpalaniraja35@gmail.com>
2026-06-12 15:09:12 +0530

    Add: find-duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+    
+       int fast = 0;
+       int slow = 0;
+
+       do{
+         slow = nums[slow];
+         fast = nums[nums[fast]];
+
+       }while(slow!=fast);  //fast and slow at the same position
+
+
+
+   slow=0; //bring back slow to index 0;So slow and fast can meet at the list cycle start point  by taking single step 
+       while(slow!=fast){
+          slow = nums[slow];
+          fast = nums[fast];
+       }
+
+          return slow;
+    }
+}
+//time complexity is o(n)     sc is o(1)
